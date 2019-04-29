079dafad470e301bfcf2ff81f881c2e0239f20e9
pablolindenberg <46655825+pablolindenberg@users.noreply.github.com>
2019-04-29 19:54:59 -0400

    Update form.php

--- form.php
+++ form.php
@@ -11,8 +11,8 @@ $message = $_POST["dmessage"];
 
 //Set SendGrid Credentials
 
-$sg_username = "Enter Your SendGrid User Name";
-$sg_password = "Enter Your SendGrid Password";
+$sg_username = "redcarpetsantiago";
+$sg_password = "sendgrid.com";
 
 // Initialize the SendGrid object with your SendGrid credentials
 
@@ -46,7 +46,32 @@ $mail->addUniqueArgument($key, $value);
 }
 try {
 
-// Add your message details using SendGrid Email object. Here The Values Are Taken By HTML Form Filled By The User.
+// Add your message details using SendGrid Email object. Here The Values Are Taken By HTML Form Filled By The User.<?php
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
 $mail->
 setFrom($senderid)->
 setSubject($subject)->
